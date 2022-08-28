40c8a96e2c497d323a2e46af1031df5cd33d055b
Maciej Chmielewski <maciej.chmielewski@rad4m.com>
2022-08-28 21:29:02 +0200

    introducing status

--- python/medium/authorized_medium.py
+++ python/medium/authorized_medium.py
@@ -1,4 +1,4 @@
-from asyncio import create_task
+from asyncio import create_task, Task, get_running_loop, Future
 from enum import Enum
 from typing import List, Optional
 from python.connector.authorized_connector import AuthorizedConnector
@@ -11,7 +11,8 @@ from python.medium.medium import Medium
 class Status(Enum):
     INITIAL = 1
     CONNECTED = 2
-    AUTHORIZED = 3
+    INTRODUCING = 3
+    AUTHORIZED = 4
 
 
 class AuthorizedClientMedium(Medium):
@@ -25,8 +26,9 @@ class AuthorizedClientMedium(Medium):
         self.status = Status.INITIAL
         self.identity_server = identity_server
         self.medium: Optional[Medium] = None
+        self.introducing = get_running_loop().create_future()
 
-        self.inner_medium_connected = create_task(self.connect(
+        self.connected = create_task(self.connect(
             target=target,
             available_source_mediums=available_source_mediums,
         ))
@@ -41,6 +43,12 @@ class AuthorizedClientMedium(Medium):
             available_source_mediums=available_source_mediums,
         ).establish_connection(target, self.receive_message)
         self.status = Status.CONNECTED
+        create_task(self.introduce())
+
+    async def introduce(self):
+        await self.medium.send(b'hi')
+        self.status = Status.INTRODUCING
+        self.introducing.set_result(None)
 
     def receive_message(self, message: bytes):
         if self.status == Status.AUTHORIZED:
--- python/tests/authorized_client_medium_tests.py
+++ python/tests/authorized_client_medium_tests.py
@@ -1,6 +1,5 @@
 import unittest
 from typing import Optional
-
 from python.core.interface_identity import InterfaceIdentity
 from python.medium.authorized_medium import AuthorizedClientMedium, Status
 from python.medium.kinds import WebsocketSourceMedium, WebsocketTargetMedium
@@ -35,21 +34,42 @@ class AuthorizedClientMediumTestCase(unittest.IsolatedAsyncioTestCase):
             identity_server=TestIdentityServer({'bob': [WebsocketTargetMedium(location=bob_websocket_location)]}),
             available_source_mediums=[WebsocketSourceMedium()],
         )
-        await medium.inner_medium_connected
+        await medium.connected
+
+        await server.sessions[0].send(b'hi')
+        self.assertEqual(medium.status, Status.CONNECTED, 'connected status should be Status.CONNECTED')
+        self.assertEqual(len(server.sessions), 1, 'on connected status, session should be established')
+
+        server.close()
+        await server_close
+
+    async def test_introducing_status(self):
+        bob_websocket_location = Location(host='localhost', port=8765)
+        server, server_close = await run_server(bob_websocket_location)
+
+        medium = AuthorizedClientMedium(
+            target=InterfaceIdentity(pseudonym='bob', interface='some_interface'),
+            identity_server=TestIdentityServer({'bob': [WebsocketTargetMedium(location=bob_websocket_location)]}),
+            available_source_mediums=[WebsocketSourceMedium()],
+        )
+        await medium.introducing
         received_message: Optional[bytes] = None
 
         def receive_message(message: bytes):
             nonlocal received_message
             received_message = message
 
-        medium.on_message = receive_message
-        await server.sessions[0].send(b'hi')
-        self.assertEqual(medium.status, Status.CONNECTED, 'connected status should be Status.CONNECTED')
-        self.assertIsNone(received_message, 'message should not be received')
+        server.sessions[0].onMessage = receive_message
+        self.assertEqual(medium.status, Status.INTRODUCING, 'introducing status should be Status.INTRODUCING')
+        self.assertIsNone(received_message, 'message should be received')
 
         server.close()
         await server_close
 
+
+# TODO: each state must reject invalid ciphers
+# TODO: each state must reject invalid signatures
+# TODO: each state must reject not expected messages
 
 if __name__ == '__main__':
     unittest.main()

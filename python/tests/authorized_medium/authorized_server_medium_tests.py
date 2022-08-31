77cdc2cf95edcbb0ab74fa22228194212c912330
Maciej Chmielewski <maciej.chmielewski@rad4m.com>
2022-08-31 14:49:51 +0200

    server initial status

--- python/tests/authorized_medium/authorized_server_medium_tests.py
+++ python/tests/authorized_medium/authorized_server_medium_tests.py
@@ -1,17 +1,25 @@
 import unittest
 
+from python.medium.authorized.server import AuthorizedServerMedium, Status
 from python.medium.websocket_medium import WebsocketMedium
 from python.websocket.client import run_client
 from python.websocket.location import Location
 from python.websocket.server import run_server
 
 
-class AuthorizedServerMediumTestCase(unittest.TestCase):
-    def test_initial_state(self):
+class AuthorizedServerMediumTestCase(unittest.IsolatedAsyncioTestCase):
+    async def test_initial_state(self):
         bob_websocket_location = Location(host='localhost', port=8765)
         server, server_close = await run_server(bob_websocket_location)
-        client, client_close = await run_client(bob_websocket_location)
-        asad = WebsocketMedium.server(server.sessions[0])
+        _ = await run_client(bob_websocket_location)
+        websocket_medium = WebsocketMedium.server(server.sessions[0])
+        medium = AuthorizedServerMedium(
+            medium=websocket_medium
+        )
+
+        self.assertEqual(Status.INITIAL, medium.status, "initially, status should be Status.INITIAL")
+        server.close()
+        await server_close
 
 
 if __name__ == '__main__':

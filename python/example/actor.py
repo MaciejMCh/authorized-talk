61c7a16142c4eff008d6e2ef92a9e6cbb5ccc772
Maciej Chmielewski <maciej.chmielewski@rad4m.com>
2022-09-05 22:28:34 +0200

    extracted actor

--- python/example/actor.py
+++ python/example/actor.py
@@ -1,4 +1,5 @@
-from typing import cast
+from asyncio import run
+from typing import cast, Callable
 from google.protobuf.message import Message
 from google.protobuf.reflection import GeneratedProtocolMessageType
 from python.blockchain.account import Account
@@ -7,7 +8,6 @@ from python.core.random import BuiltInRandom
 from python.core.rsa_keys import RsaKeys
 from python.example.drone_simulation_controller import DroneSimulationController
 from python.example.implement_interface import implement_interface
-from python.example.proto.system_pb2 import Drone, RequestTelemetryCommand, TakeOffCommand
 from python.identity_server.blockchain_identity_server import BlockchainIdentityServer
 from python.medium.authorized.server import AuthorizedServerMedium
 from python.medium.websocket_medium import WebsocketMedium
@@ -22,37 +22,42 @@ class Actor:
         identity_server_contract: IdentityServerContract,
         account: Account,
         websocket_location: Location,
+        handle_command: Callable[[Message], Message],
     ):
         self.actor_type = actor_type
         self.identity_server_contract = identity_server_contract
         self.identity_server = BlockchainIdentityServer(identity_server_contract)
         self.account = account
         self.websocket_location = websocket_location
+        self.handle_command = handle_command
         self.rsa_keys = RsaKeys.generate()
         self.drone_simulation_controller = DroneSimulationController()
         self.initialize()
 
     def initialize(self):
         self.connect_to_identity_server()
-        self.open_websocket()
+        run(self.open_websocket())
 
-    def open_websocket(self):
+    async def open_websocket(self):
         server, server_close = await run_server(self.websocket_location)
 
         def on_session_opened(session: WebsocketServerSession):
-            medium = AuthorizedServerMedium(
-                pseudonym=self.account.pseudonym,
-                medium=WebsocketMedium.server(session),
-                rsa_keys=self.rsa_keys,
-                identity_server=self.identity_server,
-                random=BuiltInRandom(),
-            )
-            interface = await medium.authorized
-            implement_interface(
-                actor_type=self.actor_type,
-                interface=interface,
-                medium=medium,
-            )
+            async def do():
+                medium = AuthorizedServerMedium(
+                    pseudonym=self.account.pseudonym,
+                    medium=WebsocketMedium.server(session),
+                    rsa_keys=self.rsa_keys,
+                    identity_server=self.identity_server,
+                    random=BuiltInRandom(),
+                )
+                interface = await medium.authorized
+                implement_interface(
+                    actor_type=self.actor_type,
+                    interface=interface,
+                    medium=medium,
+                    handle_command=self.handle_command,
+                )
+            run(do())
 
         server.handle_session_opened(on_session_opened)
 
@@ -62,13 +67,3 @@ class Actor:
             publicKey=self.rsa_keys.public_key,
             websocketLocation=self.websocket_location,
         )
-
-    def receive_command(self, command: Message):
-        if isinstance(command, RequestTelemetryCommand):
-            request_telemetry_command = cast(RequestTelemetryCommand, command)
-            return
-        if isinstance(command, TakeOffCommand):
-            take_off_command = cast(TakeOffCommand, command)
-            return
-
-        raise Exception(f"unexpected command {command}")
--- python/example/drone_simulation.py
+++ python/example/drone_simulation.py
@@ -3,16 +3,10 @@ from typing import cast
 from google.protobuf.message import Message
 from python.blockchain.account import Account
 from python.blockchain.identity_server_contract import IdentityServerContract
-from python.core.random import BuiltInRandom
-from python.core.rsa_keys import RsaKeys
-from python.example.drone_simulation_controller import DroneSimulationController, TakeOff
-from python.example.implement_interface import implement_interface
-from python.example.proto.system_pb2 import ReadTelemetry, Drone
-from python.identity_server.blockchain_identity_server import BlockchainIdentityServer
-from python.medium.authorized.server import AuthorizedServerMedium
-from python.medium.websocket_medium import WebsocketMedium
+from python.example.actor import Actor
+from python.example.drone_simulation_controller import DroneSimulationController
+from python.example.proto.system_pb2 import ReadTelemetry, Drone, TakeOff, Success
 from python.websocket.location import Location
-from python.websocket.server import run_server, WebsocketServerSession
 
 
 class DroneSimulation:
@@ -22,54 +16,20 @@ class DroneSimulation:
         account: Account,
         websocket_location: Location,
     ):
-        self.identity_server_contract = identity_server_contract
-        self.identity_server = BlockchainIdentityServer(identity_server_contract)
-        self.account = account
-        self.websocket_location = websocket_location
-        self.rsa_keys = RsaKeys.generate()
-        self.drone_simulation_controller = DroneSimulationController()
-        self.initialize()
-
-    def initialize(self):
-        self.connect_to_identity_server()
-        run(self.open_websocket())
-
-    async def open_websocket(self):
-        server, server_close = await run_server(self.websocket_location)
-
-        def on_session_opened(session: WebsocketServerSession):
-            async def do():
-                medium = AuthorizedServerMedium(
-                    pseudonym=self.account.pseudonym,
-                    medium=WebsocketMedium.server(session),
-                    rsa_keys=self.rsa_keys,
-                    identity_server=self.identity_server,
-                    random=BuiltInRandom(),
-                )
-                interface = await medium.authorized
-                implement_interface(
-                    actor_type=Drone,
-                    interface=interface,
-                    medium=medium,
-                    handle_command=self.receive_command,
-                )
-            run(do())
-
-        server.handle_session_opened(on_session_opened)
-
-    def connect_to_identity_server(self):
-        self.identity_server_contract.connect(
-            account=self.account,
-            publicKey=self.rsa_keys.public_key,
-            websocketLocation=self.websocket_location,
+        self.actor = Actor(
+            actor_type=Drone,
+            identity_server_contract=identity_server_contract,
+            account=account,
+            websocket_location=websocket_location,
+            handle_command=self.receive_command,
         )
+        self.drone_simulation_controller = DroneSimulationController()
 
     def receive_command(self, command: Message) -> Message:
         if isinstance(command, ReadTelemetry):
             request_telemetry_command = cast(ReadTelemetry, command)
-            return
         if isinstance(command, TakeOff):
-            take_off_command = cast(TakeOff, command)
-            return
+            self.drone_simulation_controller.take_off()
+            return TakeOff.Result(success=Success())
 
         raise Exception(f"unexpected command {command}")
